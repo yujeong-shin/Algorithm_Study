f0ab955870b1e0de55f4e023e35af71d5d814ba4
yujeong-shin <vouobb@naver.com>
2023-02-13 13:51:01 +0900

    ex6_4 풀이(LRU, Cache)

--- src/Main6_3.java
+++ src/Main6_3.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 
 public class Main6_3 {
+//    public int[] Solution(int n, int[] arr) {
+//        for (int i = 1; i < n; i++) {
+//            int tmp = arr[i];
+//            for (int j = i-1; j >= 0; j--) {
+//                if(tmp < arr[j]) {
+//                    arr[j+1] = arr[j];
+//                    arr[j] = tmp;
+//                }
+//                else break;
+//            }
+//        }
+//        return arr;
+//    }
+
     public int[] Solution(int n, int[] arr) {
         for (int i = 1; i < n; i++) {
-            int tmp = arr[i];
-            for (int j = i-1; j >= 0; j--) {
-                if(tmp < arr[j]) {
-                    arr[j+1] = arr[j];
-                    arr[j] = tmp;
-                }
+            int tmp = arr[i], j;
+            for (j = i-1; j >= 0; j--) {
+                if(arr[j] > tmp) arr[j+1] = arr[j];
                 else break;
             }
+            arr[j+1] = tmp;
         }
         return arr;
     }
