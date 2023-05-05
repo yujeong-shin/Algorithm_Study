ae2a5d994ae79338d9b269cc620396c36ecb7cc7
yujeong-shin <vouobb@naver.com>
2023-05-05 17:45:32 +0900

    ex8_13 복습

--- src/example.java
+++ src/example.java
@@ -8,66 +8,47 @@ class Point {
     }
 }
 class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] board, dis;
-    static int m, n;
-    static Queue<Point> Q = new LinkedList<>();
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int[][] board;
+    static int n, answer=0;
 
-    public void BFS() {
-        while(!Q.isEmpty()) {
-            Point tmp = Q.poll();
-
-            for(int i=0; i<4; i++) {
-                int nx = tmp.x + dx[i];
-                int ny = tmp.y + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
-                    board[nx][ny] = 1;
-                    Q.offer(new Point(nx, ny));
-                    dis[nx][ny] = dis[tmp.x][tmp.y]+1;
+    public void Solution() {
+        for(int i=0; i<n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(board[i][j]==1) {
+                    board[i][j]=0;
+                    DFS(i, j);
+                    answer++;
                 }
             }
         }
-
     }
+    public void DFS(int x, int y) {
+        for(int i=0; i<8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1) {
+                board[nx][ny]=0;
+                DFS(nx, ny);
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        m = sc.nextInt(); //가로칸 수
-        n = sc.nextInt(); //세로칸 수
-        board = new int[n][m];
-        dis = new int[n][m];
-
-        //정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
+        n = sc.nextInt();
+        board = new int[n][n];
         for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
+            for(int j=0; j<n; j++) {
                 board[i][j] = sc.nextInt();
-                if(board[i][j]==1) Q.offer(new Point(i, j));
-            }
-        }
-        T.BFS();
-
-        boolean flag = true;
-        int answer = Integer.MIN_VALUE;
-        //저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고,
-        //토마토가 모두 익지는 못하는 상황이면(0이 존재) -1을 출력해야 한다.
-        for(int i=0; i<n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(board[i][j]==0) flag=false;
             }
         }
 
-        if(flag) {
-            for(int i=0; i<n; i++) {
-                for (int j = 0; j < m; j++) {
-                    answer = Math.max(answer, dis[i][j]);
-                }
-            }
-            System.out.println(answer);
-        }
-        else {
-            System.out.println(-1);
-        }
+        T.Solution();
+        System.out.println(answer);
     }
 }
