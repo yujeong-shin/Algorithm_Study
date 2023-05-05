5fae0ca9f641ce7d0df6e88d68083b2b682e08c7
yujeong-shin <vouobb@naver.com>
2023-05-05 16:50:00 +0900

    ex8_12 복습

--- src/example.java
+++ src/example.java
@@ -11,17 +11,17 @@ class Main {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int[][] board, dis;
-    Queue<Point> Q = new LinkedList<>();
+    static int m, n;
+    static Queue<Point> Q = new LinkedList<>();
 
-    public void BFS(int x, int y) {
-        Q.offer(new Point(x, y));
-
+    public void BFS() {
         while(!Q.isEmpty()) {
             Point tmp = Q.poll();
+
             for(int i=0; i<4; i++) {
                 int nx = tmp.x + dx[i];
                 int ny = tmp.y + dy[i];
-                if(nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
                     board[nx][ny] = 1;
                     Q.offer(new Point(nx, ny));
                     dis[nx][ny] = dis[tmp.x][tmp.y]+1;
@@ -34,17 +34,40 @@ class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        board = new int[8][8];
-        dis = new int[8][8];
-        for(int i=1; i<=7; i++) {
-            for(int j=1; j<=7; j++) {
+        m = sc.nextInt(); //가로칸 수
+        n = sc.nextInt(); //세로칸 수
+        board = new int[n][m];
+        dis = new int[n][m];
+
+        //정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 board[i][j] = sc.nextInt();
+                if(board[i][j]==1) Q.offer(new Point(i, j));
             }
         }
-        board[1][1] = 1;
-        T.BFS(1, 1);
+        T.BFS();
 
-        if(dis[7][7]==0) System.out.println(-1);
-        else System.out.println(dis[7][7]);
+        boolean flag = true;
+        int answer = Integer.MIN_VALUE;
+        //저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고,
+        //토마토가 모두 익지는 못하는 상황이면(0이 존재) -1을 출력해야 한다.
+        for(int i=0; i<n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(board[i][j]==0) flag=false;
+            }
+        }
+
+        if(flag) {
+            for(int i=0; i<n; i++) {
+                for (int j = 0; j < m; j++) {
+                    answer = Math.max(answer, dis[i][j]);
+                }
+            }
+            System.out.println(answer);
+        }
+        else {
+            System.out.println(-1);
+        }
     }
 }
