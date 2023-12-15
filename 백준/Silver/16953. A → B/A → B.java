bc8e76131b694ac9014e51311c230cbe65d2c752
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2023-12-15 09:20:11 +0900

    [Silver II] Title: A → B, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -6,32 +6,22 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long a, b;
+    static int answer=-1;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        a = Long.parseLong(st.nextToken());
-        b = Long.parseLong(st.nextToken());
-        System.out.println(bfs());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        //System.out.println(bfs());
+        dfs(0, a, b);
+        System.out.println(answer);
     }
-    static int bfs(){
-        int cnt=0;
-        Queue<Long> q = new LinkedList<>();
-        q.add(a);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-
-            for(int i=0; i<size; i++){
-                long tmp = q.poll();
-                if(tmp==b)
-                    return cnt+1;
-
-                if(tmp*2<=b) q.add(tmp*2);
-                if(tmp*10+1<=b) q.add(tmp*10+1);
-            }
-            cnt++;
+    static void dfs(int L, long a, long b){
+        if(a>b) return;
+        if(a==b) answer=L+1;
+        else {
+            dfs(L+1, a*2, b);
+            dfs(L+1, a*10+1, b);
         }
-        return -1;
     }
 }
