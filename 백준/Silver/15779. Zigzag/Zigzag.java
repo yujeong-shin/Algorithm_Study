63ea6894af5271145b6b5812b11abc9506cdb098
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-02-28 14:02:14 +0900

    [Silver V] Title: Zigzag, Time: 156 ms, Memory: 14968 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15779. Zigzag/Zigzag.java
+++ 백준/Silver/15779. Zigzag/Zigzag.java
@@ -16,33 +16,14 @@ public class Main {
             arr[i] = num;
         }
 
-        // 배열의 0, 1 번째 요소를 가지고 초기 결과 값, 초기 flag값 설정
-        int max = 1;
-        if(N >= 2){
-            int result = 2;
-            boolean preFlag = calcFlag(arr, 0, 1);
-
-            for (int i = 1; i < N-1; i++) {
-                // 배열의 1, 2 번째 요소의 flag
-                boolean flag = calcFlag(arr, i, i+1);
-                if(preFlag == flag) { // 이전 증감 부호와 같다면 리셋
-                    max = Math.max(result, max);
-                    result=2;
-                }
-                else{ // 이전 증감 부호와 다르다면 길이 증가
-                    result++;
-                    max = Math.max(result, max);
-                }
-                preFlag = flag; // 이전 부호 값을 현재 부호 값으로 설정
-            }
-        }
-        else {
-            max = N;
+        int max = Integer.MIN_VALUE;
+        int count = 2;
+        for (int i = 0; i < N-2; i++) {
+            if(arr[i] <= arr[i+1] && arr[i+1] <= arr[i+2]) count=2;
+            else if(arr[i] >= arr[i+1] && arr[i+1] >= arr[i+2]) count=2;
+            else count++;
+            max = Math.max(max, count);
         }
         System.out.println(max);
-    }
-
-    static boolean calcFlag(int[] arr, int a, int b){
-        return arr[a] - arr[b] < 0; // 앞 숫자 - 뒤 숫자 값이 양수면 true(양수 부호)
     }
 }
