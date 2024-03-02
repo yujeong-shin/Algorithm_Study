0fc9506e620775019b69186189b3ee829345c7ca
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-03-02 10:47:51 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,44 +1,38 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-import static java.lang.System.exit;
-
 public class Main {
-    static int[] nanjangeKey;
-    static boolean[] visited;
-    static final int n=9;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        nanjangeKey = new int[n];
-        visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            nanjangeKey[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(nanjangeKey);
-        DFS(0, 0);
-    }
-    static void DFS(int L, int sum){
-        if(L==7){
-            if(sum == 100){
-                for (int i = 0; i < n; i++) {
-                    if(visited[i]){
-                        System.out.println(nanjangeKey[i]);
-                    }
-                }
-                exit(0);
-            }
-        }
-        else{
-            for (int i = 0; i < n; i++) {
-                if(!visited[i]){
-                    visited[i] = true;
-                    int s = sum + nanjangeKey[i];
-                    DFS(L+1, s);
-                    visited[i] = false;
-                }
-            }
-        }
-    }
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[9];
+		int sum=0;
+
+		for(int i=0;i<9;i++) {
+			int n = Integer.parseInt(br.readLine());
+			arr[i] = n;
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+
+		int a=0,b=0;
+        // 키 두 개를 뺐을 때 100이면 두 명 제외
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(sum - arr[i] - arr[j] == 100) {
+					a=i;
+					b=j;
+					break;
+				}
+			}
+		}
+
+		for(int i=0;i<arr.length;i++) {
+			if(i!=a && i!=b) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
 }
