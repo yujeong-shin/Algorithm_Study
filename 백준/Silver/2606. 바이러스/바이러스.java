85cd649da242ab7b24325e3b5fee1642faa314e4
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-07-23 21:25:25 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,31 +1,54 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, E, count;
-    static int[] ch;
-    static int[][] graph;
-    public static void DFS(int v) {
-        ch[v]=1;
-        for(int i=1; i<N+1; i++) {
-            if(graph[v][i]==1 && ch[i]==0) {
-                count++;
-                DFS(i);
+    static int N, M;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph;
+
+    public static int BFS(int start) {
+        int answer=0;
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(start);
+        while(!deque.isEmpty()) {
+            int cur = deque.poll();
+            // ArrayList 줄줄이 소세지 가져옴 -> !visited라면 answer++, visited=t 후, deque 추가
+            for(Integer next : graph.get(cur)) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    answer++;
+                    deque.add(next);
+                }
             }
         }
+        return answer;
     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        graph = new int[N+1][N+1];
-        ch = new int[N+1];
-        E = sc.nextInt();
-        for(int i=0; i<E; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            graph[s][e] = graph[e][s] = 1;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        visited = new boolean[N+1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
-        Main.DFS(1);
-        System.out.println(count);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        visited[1] = true;
+        System.out.println(BFS(1));
     }
 }
