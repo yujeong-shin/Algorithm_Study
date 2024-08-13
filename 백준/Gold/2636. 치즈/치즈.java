bfcd12fa92e9dde7618e82040fca40845dfde82c
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-08-13 22:19:41 +0900

    [Gold IV] Title: 치즈, Time: 156 ms, Memory: 15452 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,75 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.StringTokenizer;
+
 class Point {
     int x, y;
+
     Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
 public class Main {
     static int[] dx={-1, 0, 1, 0}, dy={0, 1, 0, -1};
-    static int n, m, time, cheese;
-    static int[][] board, countHour;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(bf.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        countHour = new int[n][m];
+    static int n, m, time;
+    static int[][] map;
+    static ArrayList<Integer> cheese;
+    static boolean[][] visited;
+    static boolean flag = true;
+    public static void BFS(int startX, int startY) {
+        int countCheese = 0;
+        visited = new boolean[n+1][m+1];
+        Deque<Point> deque = new ArrayDeque<>();
+        deque.add(new Point(startX, startY));
+        while(!deque.isEmpty()) {
+            Point cur = deque.poll();
+            for (int i = 0; i < dx.length; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j] == 1)
-                    cheese++;
-            }
-        }
-
-        int answer=0;
-        while(cheese != 0){
-            time++;
-            answer = cheese;
-            BFS(0, 0);
-        }
-
-        System.out.println(time);
-        System.out.println(answer);
-    }
-
-
-    static void BFS(int x, int y){
-        Queue<Point> Q = new LinkedList<>();
-        Q.add(new Point(x, y));
-        boolean[][] visited = new boolean[n][m];
-        visited[x][y]=true;
-
-        while (!Q.isEmpty()) {
-            Point now = Q.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny]){
+                if(nx>=0 && nx<=n && ny>=0 && ny<=m && !visited[nx][ny]) {
                     visited[nx][ny]=true;
-                    //치즈라면 녹여주고
-                    if(board[nx][ny]==1){
-                        cheese--;
-                        board[nx][ny]=0;
+                    if(map[nx][ny]==1) {
+                        map[nx][ny]=0;
+                        countCheese++;
                     }
-                    //치즈가 아니라면 확장하면서 주변 치즈탐색
-                    else {
-                        Q.add(new Point(nx, ny));
+                    else{
+                        deque.add(new Point(nx, ny));
                     }
                 }
             }
         }
+        if(countCheese==0) {
+            flag = false;
+            return;
+        }
+        time++;
+        cheese.add(countCheese);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n+1][m+1];
+        cheese = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        while(flag) {
+            BFS(0, 0);
+        }
+        System.out.println(time);
+        System.out.println(cheese.get(cheese.size()-1));
     }
 }
