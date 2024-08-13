3551ae9ff0a33a8a7dc5baed2d08746d1d136fac
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-08-13 21:28:02 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 15032 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,61 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
-class Point {
-    int x, y;
-
-    Point(int x, int y) {
+class Miro {
+    int x, y, cnt;
+    public Miro(int x, int y, int cnt){
         this.x = x;
-        this.y = y;
+        this.y =y ;
+        this.cnt = cnt;
     }
 }
-
 public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
     static int N, M;
-    static int[][] map, dis;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(bf.readLine());
-
-        N = Integer.parseInt(st.nextToken()); //4 미로 행
-        M = Integer.parseInt(st.nextToken()); //6 미로 열
-        map = new int[N+1][M+1]; // (1,1) 시작, (N,M) 도착
-        dis = new int[N+1][M+1]; // (1,1) ~ (N,M)까지 거리 count
-        for(int i=1; i<=N; i++){
-            int j=1;
-            st = new StringTokenizer(bf.readLine()); //map 정보 한줄 읽기
-            String EachRow = st.nextToken();
-            for(char ch : EachRow.toCharArray()) {
-                map[i][j] = Integer.parseInt(String.valueOf(ch)); //숫자 하나씩 map에 저장
-                j++;
+    static boolean[][] visited;
+    static int[][] map;
+    static void BFS() {
+        Deque<Miro> deque = new ArrayDeque<>();
+        visited[1][1] = true;
+        deque.add(new Miro(1, 1, 1));
+        while (!deque.isEmpty()) {
+            Miro cur = deque.poll();
+//            System.out.println("x : " + cur.x + ", y : " + cur.y + ", count : " + cur.cnt);
+            if(cur.x == N && cur.y == M) {
+                System.out.println(cur.cnt);
             }
-        }
-        map[1][1]=0;
-        dis[1][1]=1;
-        BFS(1,1);
-        System.out.println(dis[N][M]);
-    }
-    public static void BFS(int x, int y){
-        Queue<Point> Q = new LinkedList<>();
-        Q.add(new Point(x, y));
-        while(!Q.isEmpty()) {
-            Point cur = Q.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if(nx>=1 && nx<=N && ny>=1 && ny<=M && map[nx][ny]==1) {
-                    map[nx][ny]=0;
-                    Q.add(new Point(nx, ny));
-                    dis[nx][ny] = dis[cur.x][cur.y]+1; //이전 count수 + 1
+            for (int i = 0; i < dx.length; i++) {
+                int nx = dx[i] + cur.x;
+                int ny = dy[i] + cur.y;
+                if(nx>=1 && nx<=N && ny>=1 && ny<=M && !visited[nx][ny] && map[nx][ny]==1) {
+                    visited[nx][ny] = true;
+                    deque.add(new Miro(nx, ny, cur.cnt+1));
                 }
             }
         }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N+1][M+1];
+        visited = new boolean[N+1][M+1];
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = Integer.parseInt(String.valueOf(str.charAt(j-1)));
+            }
+        }
+        BFS();
     }
 }
