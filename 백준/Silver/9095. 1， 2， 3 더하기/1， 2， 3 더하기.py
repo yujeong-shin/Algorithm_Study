0a7097408a5218c0848c6cd9bed5c8a2465ab079
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-09-02 15:17:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,12 @@
+tc = int(input())
+
+for t in range(tc):
+    n = int(input())
+    dp = [0] * 11
+    dp[1]=1
+    dp[2]=2
+    dp[3]=4
+    
+    for i in range(4, 11):
+        dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
+    print(dp[n])
