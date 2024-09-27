c6ce0bf0b9e0945a43d80f0df39ee166811dd4b9
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-09-27 17:34:42 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+
+visited = [0] * n
+answer = [0]*m
+answers = []
+def DFS(level, start):
+    global n, m, numbers, visited, answer, answers
+    if level==m:
+        answers.append(answer.copy())
+        return
+    else:
+        for i in range(start, n):
+            answer[level] = numbers[i]
+            DFS(level+1, i)
+DFS(0, 0)
+
+unique_answers = set(map(tuple, answers))
+answers = list(map(list, unique_answers))
+answers.sort()
+for i in range(len(answers)):
+    answer_string=''
+    answer_string = ' '.join(map(str, answers[i]))
+    print(answer_string)
