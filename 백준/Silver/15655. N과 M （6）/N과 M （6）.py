2db0ab056fae0d36a5a0b9c010c88c9ce38199d1
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-09-27 15:58:36 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+
+answer = [0]*m
+def DFS(level, start):
+    global n, m, numbers, answer
+    if level==m:
+        answer_string=''
+        answer_string = ' '.join(map(str, answer))
+        print(answer_string)
+        return
+    else:
+        for i in range(start, n):
+            answer[level] = numbers[i]
+            DFS(level+1, i+1)
+DFS(0, 0)
