fcd181ea1764f4586669c025502ee5cc28e9b8ce
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-10-02 10:57:47 +0900

    [Silver II] Title: 쇠막대기, Time: 100 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+question = input()
+deque = deque()
+answer = 0
+for i in range(len(question)):
+    cur = question[i]
+    if i == 0:
+        deque.append(cur)
+        continue
+    else:
+        if cur == ')':
+            prev = question[i-1]
+            if prev == '(':
+                deque.pop()
+                if len(deque) != 0:
+                    answer += len(deque)
+            else:
+                deque.pop()
+                answer += 1
+        else :
+            deque.append(cur)
+print(answer)
