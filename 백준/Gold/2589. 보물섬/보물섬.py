86df775f8c5ee86d7e7fb7032f3346a4d7544e89
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-10-08 11:14:48 +0900

    [Gold V] Title: 보물섬, Time: 720 ms, Memory: 174264 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,37 +1,43 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
- 
-r, c = map(int, input().split())
-board = [list(input().rstrip()) for _ in range(r)]
- 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
- 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    visited = [[-1] * c for _ in range(r)]
-    visited[x][y] = 0
-    cnt = 0
- 
-    while q:
-        x, y = q.popleft()
- 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
- 
-            if 0 <= nx < r and 0 <= ny < c and visited[nx][ny] == -1:
-                if board[nx][ny] == "L":
-                    q.append((nx, ny))
-                    visited[nx][ny] = visited[x][y] + 1
-                    cnt = max(cnt, visited[nx][ny])
-    return cnt
- 
-result = 0
-for x in range(r):
-    for y in range(c):
-        if board[x][y] == "L":
-            result = max(result, bfs(x, y))
-print(result)
+
+n, m = map(int, input().split())
+maps = []
+for i in range(n):
+    maps.append(list(input().strip()))
+
+lands = []
+for i in range(n):
+    for j in range(m):
+        if maps[i][j] == 'L':
+            lands.append([i, j, 0])
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+deque = deque()
+answer = -sys.maxsize
+def BFS(start):
+    global answer
+    visited = [[0]*m for _ in range(n)]
+    deque.append(start)
+    visited[start[0]][start[1]] = 1
+    
+    while deque:
+        cur = deque.popleft()
+        count = cur[2]
+        if count > answer :
+            answer = count
+        
+        for i in range(len(dx)):
+            next_x = cur[0] + dx[i]
+            next_y = cur[1] + dy[i]
+            if next_x>=0 and next_x<n and next_y>=0 and next_y<m and maps[next_x][next_y]=='L':
+                if visited[next_x][next_y]==0:
+                    visited[next_x][next_y]=1
+                    deque.append([next_x, next_y, count+1])
+
+for land in lands:
+    BFS(land)
+
+print(answer)
