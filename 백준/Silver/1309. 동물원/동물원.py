5d22dd93658a5b23ca7dc4d47be837829c838a40
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2024-12-31 11:03:33 +0900

    [Silver I] Title: 동물원, Time: 72 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0]*(n+1)
+dp[0] = 1
+dp[1] = 3
+
+if n >= 2:
+    for i in range(2, n+1):
+        dp[i] = (2*dp[i-1] + dp[i-2]) % 9901
+print(dp[n])
