48af3e21a9f5529f1c9cf1a247aa17bc17a6e892
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2025-02-24 17:22:21 +0900

    [Gold IV] Title: 사탕 가게, Time: 1228 ms, Memory: 110868 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.py
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.py
@@ -1,19 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-while True:
+while(True):
     n, m = map(float, input().split())
-    n, m = int(n), int(m * 100 + 0.5)
-
+    n, m = int(n), int(m*100 + 0.5) # rounding error
     if n == 0 and m == 0:
         break
-
-    dp = [0 for _ in range(m+1)]
-    for _ in range(n):
+        
+    dp = [0] * (m+1)
+    for i in range(n):
         c, p = map(float, input().split())
-        c, p = int(c), int(p * 100 +  0.5)
-
-        for i in range(p, m+1):
-            dp[i] = max(dp[i], dp[i-p] + c)
-
+        cost, price = int(c), int(p*100+0.5)
+        for j in range(price, m+1):
+            dp[j] = max(dp[j], dp[j-price]+cost)
     print(dp[m])
