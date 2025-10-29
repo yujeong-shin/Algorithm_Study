ddbf3652b74ebeed05e43bc01c407542b39826a8
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2025-10-29 21:19:54 +0900

    [level 2] Title: 타겟 넘버, Time: 432.26 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,6 +2,23 @@ from collections import deque
 
 def solution(numbers, target):
     answer = 0
+    
+    def dfs(idx, tmp_sum):
+        nonlocal answer
+        if idx == len(numbers):
+            if tmp_sum == target:
+                answer += 1
+            return
+                
+        dfs(idx+1, tmp_sum+numbers[idx])
+        dfs(idx+1, tmp_sum-numbers[idx])
+        
+    dfs(0, 0)
+    return answer
+
+'''
+def solution(numbers, target):
+    answer = 0
     queue = deque()
     queue.append((0, numbers[0]))
     queue.append((0, -numbers[0]))
@@ -17,3 +34,4 @@ def solution(numbers, target):
             queue.append((i+1, tmp_sum - numbers[i+1]))
     
     return answer
+'''
