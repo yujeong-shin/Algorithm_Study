7760da7bae7a0ac9ee739af37b4ed0a9c1834f5a
yujeong-shin <57553339+yujeong-shin@users.noreply.github.com>
2025-12-04 11:13:57 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.10 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -9,14 +9,12 @@ def solution(bandage, health, attacks):
         attack_time, attack_dmg = attack
         no_attack_time = attack_time - 1 - cur_time
         print("no_attack_time: " + str(no_attack_time))
-        if no_attack_time >= bandage[0]: # 추가 회복
+        if no_attack_time >= bandage[0]: # Bonus
             cur_health += (no_attack_time*bandage[1])
-            # 시전 시간마다 추가 회복 진행.
-            # 예시 1번에서 10초동안 공격받지 않았다면 5*2만큼 추가 회복
             cur_health += (no_attack_time//bandage[0])*bandage[2]
         else:
             cur_health += (no_attack_time*bandage[1])
-        cur_health = min(cur_health, health) # 체력 max 넘지 않도록 조정
+        cur_health = min(cur_health, health)
         
         cur_time = attack_time
         cur_health -= attack_dmg
